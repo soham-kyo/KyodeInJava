adad5b5636802a108bc1f3ce0e1c228e85c426fb
soham-kyo <sohamvpatil07@gmail.com>
2025-09-14 15:07:21 +0530

    Add HelloWorld.java

---
+++ fundamentals/basics/HelloWorld.java
@@ -0,0 +1,8 @@
+package fundamentals.basics;
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("Welcome to KyodeInJava");
+    }
+}
