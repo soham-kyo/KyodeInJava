aa4b0a16ec4ea2fc8d12225bc64309723bb77da9
soham-kyo <sohamvpatil07@gmail.com>
2025-09-16 09:03:26 +0530

    Add Subtraction of Two Numbers

---
+++ fundamentals/basics/SubTwoNumbers.java
@@ -0,0 +1,14 @@
+package fundamentals.basics;
+
+public class SubTwoNumbers
+{
+    public static void main(String[] agrs)
+    {
+        int X = 30;
+        int Y = 79;
+        int Sub = X - Y;
+        System.out.println("First Number:" + X);
+        System.out.println("Second Number:" + Y);
+        System.out.println("Subtraction of Two Numbers:" + Sub);
+    }
+}
