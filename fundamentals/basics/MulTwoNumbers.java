fce848960c99adb296a1909893289d4bf30dca69
soham-kyo <sohamvpatil07@gmail.com>
2025-09-16 09:11:44 +0530

    Add Program for Multiplication of Two Numbers

---
+++ fundamentals/basics/MulTwoNumbers.java
@@ -0,0 +1,14 @@
+package fundamentals.basics;
+
+public class MulTwoNumbers
+{
+    public static void main(String[] args)
+    {
+        int P = 99;
+        int R = 99;
+        int Mul = P*R;
+        System.out.println("First Number:" + P);
+        System.out.println("Second Number:" + R);
+        System.out.println("Multiplication of Two Numbers:" + Mul);
+    }
+}
