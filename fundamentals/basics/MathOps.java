70e0a168652a0206edde30e29c2056a2ad122f28
soham-kyo <sohamvpatil07@gmail.com>
2025-09-17 22:36:02 +0530

    Add program for Mathematical operations

---
+++ fundamentals/basics/MathOps.java
@@ -0,0 +1,17 @@
+package fundamentals.basics;
+
+public class MathOps
+{
+    public static void main(String[] args)
+    {
+        float A = 90.9F;
+        float B = 99.1F;
+        float Add = A + B;
+        float Sub = A - B;
+        System.out.println("First Number:" + A + "\nSecond Number:" + B);
+        System.out.println("Addition:" + Add);
+        System.out.println("Subtraction:" + Sub);
+        System.out.println("Multiplication:" + A * B);
+        System.out.println("Divsion:" + A / B);
+    }
+}
