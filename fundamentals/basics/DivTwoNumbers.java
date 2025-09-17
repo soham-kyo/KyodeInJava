09a013244ec0e688e78c3a695401f787bd5740c2
soham-kyo <sohamvpatil07@gmail.com>
2025-09-17 06:16:13 +0530

    Add program : Division of Two Numbers

---
+++ fundamentals/basics/DivTwoNumbers.java
@@ -0,0 +1,20 @@
+package fundamentals.basics;
+
+public class DivTwoNumbers
+{
+    public static void main(String[] args)
+    {
+        float C = 78;
+        float D = 9;
+        float Div = C / D;
+        System.out.println("First Number:" + C);
+        System.out.println("Second Number:" + D);
+        System.out.println("Division of Two Numbers:" + Div);
+    }
+}
+
+/* Output:
+First Number:78.0
+Second Number:9.0
+Division of Two Numbers:8.666667
+ */
