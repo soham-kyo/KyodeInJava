321223e4ce8e0e8bd0e9479d2eb6037fcc7d79f8
soham-kyo <sohamvpatil07@gmail.com>
2025-09-20 14:44:15 +0530

    Add program for division of two numbers

---
+++ fundamentals/basics/Div.java
@@ -0,0 +1,23 @@
+package fundamentals.basics;
+import java.util.Scanner;
+
+public class Div
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        int A = sc.nextInt();
+        System.out.println("Enter Second Number:");
+        int B = sc.nextInt();
+        int Div = A / B;
+        System.out.println("The Division is:" + Div);
+    }
+}
+/* Output:
+Enter First Number:
+20
+Enter Second Number:
+10
+The Division is:2
+ */
