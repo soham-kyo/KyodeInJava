beedf83e2cfd956e5d7dbf3eecbf187d0ca1e729
soham-kyo <sohamvpatil07@gmail.com>
2025-09-21 08:20:48 +0530

    Add program for multiplication of two numbers

---
+++ fundamentals/basics/Mul.java
@@ -0,0 +1,23 @@
+package fundamentals.basics;
+import java.util.Scanner;
+
+public class Mul
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        int x = sc.nextInt();
+        System.out.println("Enter Second Number:");
+        int y = sc.nextInt();
+        int mul = x * y;
+        System.out.println("The Multiplication is:" + mul);
+    }
+}
+/* Output:
+Enter First Number:
+11
+Enter Second Number:
+7
+The Multiplication is:77
+ */
